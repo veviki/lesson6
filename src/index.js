c772f7948ec9664c5fae699cc18714847c3719ae
veviki <eshunvictricia840@gmail.com>
2024-06-24 08:53:34 -0700

    added new files to lesson6

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// alert('Thank God you are alive');
+//  let name1 = prompt('What is your name');
+//  alert(name1);
+//  alert("Hi " + name1 + ", welcome to our website");
+//  let name = prompt("How are you faring");
+//  alert(name);
+//  let name2 = prompt("tell me your country");
+//  alert(name2);
+
+//  let sentence = "Hi "+"there"
+//  alert(sentence);
+// let age = 18;
+// let hometown = "Axim";
+// alert(`Hi ${name1},from ${hometown}, who is ${age}, years old, welcome to our website`);
+
+// let title = prompt("What is your title");
+// let name2 = prompt("What is your name");
+// let favproduct = prompt("What is your favproduct");
+// alert(`Hi ${title}, ${name2}, you like ${favproduct}`);
+ 
+let name ="Elpe";
+name = "Kafui";
+name = "Viki";
+alert(name);
